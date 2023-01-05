10e2c753ca132d553eff309830f0bfc4d645a063
levYatsishin <lev.yatsishin@gmail.com>
2023-01-05 18:36:43 +0300

    - Image db api implemented - minor fixes

--- db_api/images_api.py
+++ db_api/images_api.py
@@ -11,14 +11,12 @@ class ImageApi(Singleton):
     def connect(self, address, access_key, secret_key):
         self.minio_client = Minio(address, access_key=access_key, secret_key=secret_key, secure=False)
 
-    def upload_image(self, image_data: bytes) -> str | None:
+    def upload_image(self, image_data: bytes, image_name: str) -> str | None:
         """
         This function uploads image
 
         :return: If successful id of uploaded image else None
         """
-
-        image_name = 1
 
         try:
             self.minio_client.put_object('images', image_name, image_data, len(image_data))
@@ -28,16 +26,16 @@ class ImageApi(Singleton):
             logger.error(f"Error: {e}\n Traceback: {traceback.format_exc()}")
             return None
 
-    def get_image(self, image_id: str) -> bytes | None:
+    def get_image(self, image_name: str) -> bytes | None:
         """
         This function returns image from minio db by link
 
-        :param image_id: Image name in minio db
+        :param image_name: Image name in minio db
         :return: Image to render
         """
 
         try:
-            data = self.minio_client.get_object('images', image_id)
+            data = self.minio_client.get_object('images', image_name)
             return data
 
         except Exception as e:
